8bd510fce02b77e5acb0a4f1d488dd4236cd741a
daguilar2004 <118065951+daguilar2004@users.noreply.github.com>
2024-11-16 19:53:39 -0600

    tailwind configuration fix

--- src/components/common/Menu.js
+++ src/components/common/Menu.js
@@ -5,18 +5,18 @@ const Menu = () => {
 
   // default state for a user, values to be rendered on the menu component
   const [user, setUser] = useState({
-    username: "default_user",
+    username: "default_user", 
     firstName: "First",
     lastName: "Last"
   })
 
   return (
-
+ 
     // root div of the component should define the overall style of the container itself
     <div className='w-96 p-6 flex flex-col items-center bg-neutral-700 rounded-xl'>
 
       {/* import placeholder profile picture for now */}
-      <img className="w-36 mb-4 rounded-full " src="https://www.shutterstock.com/image-vector/blank-avatar-photo-place-holder-600nw-1095249842.jpg" />
+      <img className="w-36 mb-4 rounded-full " src="https://plus.unsplash.com/premium_photo-1663061406443-48423f04e73d?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8Y29tbXVuaXR5JTIwc2VydmljZXxlbnwwfHwwfHx8MA%3D%3D" />
       
       {/* render user info from the user state */}
       <h2><b>{user.username}</b></h2>
