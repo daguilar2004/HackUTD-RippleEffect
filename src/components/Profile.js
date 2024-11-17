6224b334fb1aaf1c22df555a9b50215779ba1e41
dino-cowboy <jxo230022@utdallas.edu>
2024-11-17 10:59:13 -0600

    meep

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -5,41 +5,28 @@ const Profile = () => {
     <div>
       <div>
         <img
-          className="pl-2 pb-4 w-22 h-20"
+          className="pl-2 pb-4 w-22 h-20 "
           src="/PINIT.png"
           alt="Description"
         />
-        <img
-          className="w-12 mb-4 rounded-full pfp"
-          src="https://media.licdn.com/dms/image/v2/D5603AQEX0EO9e6DhhQ/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1727487579399?e=1737590400&v=beta&t=bhZMsnc3VAmoTILSgQfu08K0IktCnRkNIzZAwn5kXtM"
-          alt="Profile"
-        />
-        <img
-          className="bg-image"
-          src="/Pattern.png"
-          alt="Pattern Background"
-        />
-      </div>
-
-      <div className="vertical">
-        <img
-          className="container-image rounded-full"
-          src="https://media.licdn.com/dms/image/v2/D5603AQEX0EO9e6DhhQ/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1727487579399?e=1737590400&v=beta&t=bhZMsnc3VAmoTILSgQfu08K0IktCnRkNIzZAwn5kXtM"
-          alt="Profile"
-        />
-        <div className="name">
-          <h2>Container 1</h2>
-          <p>This is the first container with text beside the image.</p>
+           <div className="background-image">
+        {/* Containers */}
+        <div className="vertical">
+        <img class="w-36 rounded-full" src= "https://media.licdn.com/dms/image/v2/D5603AQEX0EO9e6DhhQ/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1727487579399?e=1737590400&v=beta&t=bhZMsnc3VAmoTILSgQfu08K0IktCnRkNIzZAwn5kXtM" />
+          <h2>Damian Aguillar</h2>
+          <p1>Completed 45 hours total.</p1>
+          <button class="edit">Edit Profile</button>
+        </div>
+        <div className="vertical">
+          <h3>Container 2</h3>
+          <p>This is the second container.</p>
+          <button class="view">View Waivers</button>
+        </div>
+        <div className="vertical">
+          <p>Your Pinboard</p>
         </div>
       </div>
-      <div className="vertical">
-        <h2>Container 2</h2>
-        <p>This is the second container.</p>
-      </div>
-      <div className="vertical">
-        <h2>Container 3</h2>
-        <p>This is the third container.</p>
-      </div>
+    </div>
     </div>
   );
 };
